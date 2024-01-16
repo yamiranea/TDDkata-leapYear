737b2a2c55b3bc768614ad9f4b12690d1f64f316
yamiranea <yamilarenea@gmail.com>
2024-01-16 09:26:48 +0100

    LeapYear.test.js refactored using a unique test for both required scenarios

--- tests/LeapYear.test.js
+++ tests/LeapYear.test.js
@@ -2,27 +2,20 @@ const { expect } = require("@jest/globals");
 const LeapYear = require("../LeapYear");
 
 describe("Test to validate whether a year is a leap year or not", () => {
-  test("Test that returns true if year is divisible by 4 and 400", () => {
-    const years = [4, 400];
-    const expected_response = true;
-    const leapYear = new LeapYear();
+  const testCases = [
+    { years: [4, 400], expected_response: true },
+    { years: [6, 100], expected_response: false },
+  ];
 
-    const received_response = years.every((year) =>
-      leapYear.validate_leap_year(year)
-    );
-
-    expect(received_response).toBe(expected_response);
-  });
-
-  test("Test that returns false if year is NOT divisible by 4", () => {
-    const years = [6, 100];
-    const expected_response = false;
-    const leapYear = new LeapYear();
-
-    const received_response = years.every((year) =>
-      leapYear.validate_leap_year(year)
-    );
-
-    expect(received_response).toBe(expected_response);
+  testCases.forEach(({ years, expected_response }) => {
+    test(`Test that returns ${expected_response} if year is ${years.join(
+      " and "
+    )} divisible`, () => {
+      const leapYear = new LeapYear();
+      const received_response = years.every((year) =>
+        leapYear.validate_leap_year(year)
+      );
+      expect(received_response).toBe(expected_response);
+    });
   });
 });
